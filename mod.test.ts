e874b9ae6e758d8e0685a7fda662ecb34d30ae02
Gaute Rongve <gaute.rongve@gmail.com>
2024-12-06 21:01:25 +0100

    feat: add slugoptions

--- mod.test.ts
+++ mod.test.ts
@@ -4,3 +4,11 @@ import { sluggo } from './mod.ts'
 Deno.test('that it works with a simple string', () => {
   assertEquals(sluggo('Hello World'), 'hello-world')
 })
+
+Deno.test('that it works with a simple string and a custom separator', () => {
+  assertEquals(sluggo('Hello World', { separator: '_' }), 'hello_world')
+})
+
+Deno.test('that it works with a simple string and uppercase', () => {
+  assertEquals(sluggo('Hello World', { uppercase: true }), 'HELLO-WORLD')
+})
--- mod.ts
+++ mod.ts
@@ -1,3 +1,15 @@
-export function sluggo(string: string): string {
-  return string.toLowerCase().replace(/\s/g, '-')
+type SlugOptions = {
+  separator?: string
+  uppercase?: boolean
+}
+
+export function sluggo(string: string, options?: SlugOptions): string {
+  const separator = options?.separator || '-'
+  const uppercase = options?.uppercase || false
+
+  if (uppercase) {
+    return string.toUpperCase().replace(/\s/g, separator)
+  }
+
+  return string.toLowerCase().replace(/\s/g, separator)
 }
